96b658942d77b69b3f0c28fc494406bf594ec2d2
Joel Parkinson <joelparkinson@live.co.uk>
2020-08-08 20:10:52 +0100

    Working Connection, Get & Create

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using BeetleX.Redis;
+using System;
+using System.Threading.Tasks;
 
 namespace Redis_Learning
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var redisStuff = new RedisStuff();
+            var text = await redisStuff.GetTest();
+
+            if (text is null)
+            {
+                Console.WriteLine("IT FAILED!");
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+
+            await redisStuff.CreateTest("Test Input");
+            Console.WriteLine("YAY");
+            Console.ReadLine();
+        }
+    }
+
+    internal class RedisStuff
+    {
+        public RedisStuff()
+        {
+            Redis.Default.DataFormater = new JsonFormater();
+            Redis.Default.Host.AddWriteHost("192.168.0.17").Password = "pass123";
+        }
+
+        internal async Task<string> GetTest()
+        {
+            return await Redis.Get<string>("test");
+        }
+
+        internal async Task CreateTest(string input)
+        {
+            await Redis.Set("Test Create", input);
         }
     }
 }
